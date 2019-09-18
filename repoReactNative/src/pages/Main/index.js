7e16d14f03ec33d87d053221f618314b946f789c
Hugo <hugohoffmann04@gmail.com>
2019-09-18 10:49:07 -0300

    new: style to form

--- repoReactNative/src/pages/Main/index.js
+++ repoReactNative/src/pages/Main/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from './styles';
+import { Containerm, Form, Input, SubmiButton } from './styles';
 
 export default function Main() {
   return (
-    <Container />
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeHolder="user/repo"
+        />
+        <SubmiButton>
+          <Icon  name="add" size={20} />
+        </SubmiButton>
+      </Form>
+    </Container>
   );
 }
 
